92073fac0fcda3c2b2d9dc9c4f52eeea2a742876
mkoty1 <mkoty@yandex.ru>
2016-06-14 02:37:32 +0300

    Adapted for github pages

--- app.js
+++ app.js
@@ -3,14 +3,8 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-        'ngRoute',
         'ui.bootstrap'
     ])
-    .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-        $locationProvider.hashPrefix('!');
-
-        $routeProvider.otherwise({redirectTo: '/view1'});
-    }])
     .controller('myAppController', myAppController);
 
 myAppController.$inject = ["$scope"];
